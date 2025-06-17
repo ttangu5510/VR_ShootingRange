2149264ba0df95dda468eda57b4a4342e6f947ba
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-06-17 17:53:36 +0900

    [Feat] Add Sniper scope
    - 스나이퍼 스코프를 RenderTexture + 카메라 + Shader로 구현.
    - FOV값을 작게 설정해 확대한 렌더 텍스쳐를 Quad 오브젝트에 적용 후, Shader를 원형으로만 적용하게 스크립트를 작성후 적용. 카메라는 스나이퍼 스코프 앞에, 렌더텍스쳐된 오브젝트는 스코프 뒤(눈앞)에 배치

---
+++ Assets/Scripts/Sniper.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper : Gun
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
